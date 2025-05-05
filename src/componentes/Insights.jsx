15fb33f45ee87b2ab80936e38aa1237e0b5090a2
Natan Corcovia <natankorkov@gmail.com>
2025-05-05 16:48:59 -0300

    Update Insights.jsx

--- src/componentes/Insights.jsx
+++ src/componentes/Insights.jsx
@@ -3,9 +3,9 @@ export default function Insights({ img , title , subtitle , description}){
         <div>
             <img src={img} className="w-[250px]" />
             <h1 className="text-7xl font-bold my-9">{title}</h1>
-            <hr className="w-[500px]  border-gray-400"/>
-            <h2>{subtitle}</h2>
-            <p>{description}</p>
+            <hr className="w-[500px] border-gray-400"/>
+            <h2 className="font-bold text-[#5C5A5A] text-[20px]">{subtitle}</h2>
+            <p className="text-[#5C5A5A]">{description}</p>
         </div>
     )
 }
